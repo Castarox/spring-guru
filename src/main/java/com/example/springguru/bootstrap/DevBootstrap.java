85c5f3ed70291c55fe8a80ee04856c2744087496
castarox <mika.kamil12@gmail.com>
2017-10-09 08:48:57 +0200

    Switch publisher attribute in Book class to Publisher instead of String

--- src/main/java/com/example/springguru/bootstrap/DevBootstrap.java
+++ src/main/java/com/example/springguru/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.example.springguru.bootstrap;
 
 import com.example.springguru.model.Author;
 import com.example.springguru.model.Book;
+import com.example.springguru.model.Publisher;
 import com.example.springguru.repositories.AuthorRepository;
 import com.example.springguru.repositories.BookRepository;
+import com.example.springguru.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,24 +16,33 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository,
+                        BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData(){
 
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("title", "1234", "ER");
+        Publisher er = new Publisher("er");
+        publisherRepository.save(er);
+        Book ddd = new Book("title", "1234", er);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Hon");
-        Book noEjb = new Book("Ejb", "12346", "Hello");
+        Publisher hello = new Publisher("Hello");
+        publisherRepository.save(hello);
+        Book noEjb = new Book("Ejb", "12346", hello);
         rod.getBooks().add(noEjb);
         noEjb.getAuthors().add(rod);
 
