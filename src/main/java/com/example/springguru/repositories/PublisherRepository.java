cb3461447bb6fff1e220edfdd630e675e3ea3479
castarox <mika.kamil12@gmail.com>
2017-10-09 08:48:09 +0200

    Create repository Publisher class

---
+++ src/main/java/com/example/springguru/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.example.springguru.repositories;
+
+import com.example.springguru.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+}
